fbf112c6d51db1331b50c14c01e10d3ae398fcec
swingswastaken <diego.martincaro@gmail.com>
2026-03-06 19:44:49 +0100

    fix display

--- coinflips.py
+++ coinflips.py
@@ -410,14 +410,14 @@ async def skill_list_cmd(interaction: discord.Interaction):
         normal_coins = s["coins"] - s["unbreakable"]
 
         coin_display = (
-            f"{HEAD} " * normal_coins +
+            f"{TAIL} " * normal_coins +
             f"{UNBREAKABLE_HEAD} " * s["unbreakable"]
         )
 
         lines.append(
             f"**ID `{s['user_skill_id']}`**\n"
             f"{s['skill_name']}\n" 
-            f"{s['base_power']} Base Power & {s['coin_power']} Coin Power\n"
+            f"{s['base_power']} Base Power {s['coin_power']} Coin Power\n"
             f"{coin_display}"
         )
 
